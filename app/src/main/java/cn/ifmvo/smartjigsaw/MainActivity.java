8e5e3962b0fd55ed52c04d39ec7b74e9c06067e7
Matthew_Chen <Matthew_Chen_1994@163.com>
2020-09-23 16:50:50 +0800

    nothing

--- app/src/main/java/cn/ifmvo/smartjigsaw/MainActivity.java
+++ app/src/main/java/cn/ifmvo/smartjigsaw/MainActivity.java
@@ -47,8 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new BaseQuickAdapter<ImgItem, BaseViewHolder>(R.layout.list_item_img) {
             @Override
-            protected void convert(BaseViewHolder helper, ImgItem item)
-            {
+            protected void convert(BaseViewHolder helper, ImgItem item) {
                 ImageView ivImage = helper.getView(R.id.iv_img);
                 helper.setImageResource(R.id.iv_img, item.imgResId);
                 ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
@@ -60,12 +59,11 @@ public class MainActivity extends AppCompatActivity {
 
         adapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
-            public void onItemClick(BaseQuickAdapter adapter, View view, int position)
-            {
-                for (int i=0; i<imgResList.size(); i++){
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                for (int i = 0; i < imgResList.size(); i++) {
                     ImgItem item = imgResList.get(i);
                     ImgItem clickItem = imgResList.get(position);
-                    if (Math.abs(item.x - clickItem.x) + Math.abs(item.y - clickItem.y) == 1 && item.imgResId == 0){
+                    if (Math.abs(item.x - clickItem.x) + Math.abs(item.y - clickItem.y) == 1 && item.imgResId == 0) {
 
                     }
                 }
@@ -81,25 +79,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void initData(){
-        for (int i=0; i<count*count; i++){
+    private void initData() {
+        for (int i = 0; i < count * count; i++) {
             int id = 0;
-            if (i != count*count - 1){
-                id = getDrawableId(String.format("android_0%d", i+1));
+            if (i != count * count - 1) {
+                id = getDrawableId(String.format("android_0%d", i + 1));
             }
-            imgResList.add(new ImgItem(i%count, i/count, id, i));
+            imgResList.add(new ImgItem(i % count, i / count, id, i));
         }
     }
 
-    public static int getDrawableId(String name)
-    {
+    public static int getDrawableId(String name) {
         int result = -1;
-        try
-        {
+        try {
             Class clz = Class.forName(R.drawable.class.getName());
             Field field = clz.getField(name);
             result = field.getInt(new R.drawable());
-        } catch (Exception ignored) { }
+        } catch (Exception ignored) {
+        }
         return result;
     }
 }
